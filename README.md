31ff1443388634f3373e940edc165e85e9453c51
Zifeng "Enrique" Su <suzihitaki@gmail.com>
2019-04-26 14:05:25 -0400

    Finished clipboard Debug

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # atom-mdhelper package
 
 Atom-mdhelper is an atom package for markdown user, with autocomplete and snippet feature.
-
-![alttext](logo.png)
--- spec/atom-mdhelper-spec.js
+++ spec/atom-mdhelper-spec.js
@@ -70,4 +70,114 @@ describe('AtomMdhelper', () => {
       });
     });
   });
+  describe('when the atom-mdhelper:table event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.atom-mdhelper')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'atom-mdhelper:table');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.atom-mdhelper')).toExist();
+
+        let atomMdhelperElement = workspaceElement.querySelector('.atom-mdhelper');
+        expect(atomMdhelperElement).toExist();
+
+        let atomMdhelperPanel = atom.workspace.panelForItem(atomMdhelperElement);
+        expect(atomMdhelperPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'atom-mdhelper:table');
+        expect(atomMdhelperPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.atom-mdhelper')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'atom-mdhelper:table');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let atomMdhelperElement = workspaceElement.querySelector('.atom-mdhelper');
+        expect(atomMdhelperElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'atom-mdhelper:table');
+        expect(atomMdhelperElement).not.toBeVisible();
+      });
+    });
+  });
+  describe('when the atom-mdhelper:list event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.atom-mdhelper')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'atom-mdhelper:list');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.atom-mdhelper')).toExist();
+
+        let atomMdhelperElement = workspaceElement.querySelector('.atom-mdhelper');
+        expect(atomMdhelperElement).toExist();
+
+        let atomMdhelperPanel = atom.workspace.panelForItem(atomMdhelperElement);
+        expect(atomMdhelperPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'atom-mdhelper:list');
+        expect(atomMdhelperPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.atom-mdhelper')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'atom-mdhelper:list');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let atomMdhelperElement = workspaceElement.querySelector('.atom-mdhelper');
+        expect(atomMdhelperElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'atom-mdhelper:list');
+        expect(atomMdhelperElement).not.toBeVisible();
+      });
+    });
+  });
 });
